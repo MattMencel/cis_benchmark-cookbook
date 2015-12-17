60e50e8eccef1bceb1153f1ffd61a33ff37aa508
Mihai Petracovici <m-petracovici@wiu.edu>
2015-12-17 15:48:51 -0600

    Update sysctl version

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,10 @@ license 'Apache 2.0'
 description "Applies the Center for Internet Security's Benchmarks at\
  configuration recommendations Level-I."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.16'
+version '0.0.17'
 
 %w(redhat centos fedora scientific debian ubuntu).each do |os|
   supports os
 end
 
-depends 'sysctl', '~> 0.6.0'
+depends 'sysctl', '~> 0.7.0'
